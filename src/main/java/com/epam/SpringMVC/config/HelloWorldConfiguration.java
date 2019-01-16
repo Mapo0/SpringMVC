75ce6b5abf8f4d3d210fc4c8234e077ec0c01f7d
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2019-01-16 13:00:30 +0400

    mvc

---
+++ src/main/java/com/epam/SpringMVC/config/HelloWorldConfiguration.java
@@ -0,0 +1,17 @@
+package com.epam.SpringMVC.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class HelloWorldConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldConfiguration.class, args);
+    }
+
+}
