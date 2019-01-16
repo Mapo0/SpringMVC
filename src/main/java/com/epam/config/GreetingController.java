15fd60c66c22e9e54ecdf8f093ed8403a6508057
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2019-01-16 16:56:06 +0400

    add controller2

--- src/main/java/com/epam/config/GreetingController.java
+++ src/main/java/com/epam/config/GreetingController.java
@@ -1,18 +1,22 @@
 package com.epam.config;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
+
 
 @Controller
 public class GreetingController {
 
     @RequestMapping("/index")
-    public String index (Map<String, Object> model){
-        return "index";
+    public String welcome() {
+        return "login";
     }
+    @RequestMapping("/foo")
+    public String foo(Map<String, Object> model) {
+        throw new RuntimeException("Foo");
+    }
+
 
 }
--- src/main/webapp/WEB-INF/jsp/error.jsp
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -1,4 +1,6 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page isELIgnored ="false" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
